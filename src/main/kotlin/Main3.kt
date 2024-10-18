089eb39e4037da0190c83f66e0ff04a1f3a7db0d
first <murdasovigor17@gmail.com>
2024-10-18 13:08:54 +0300

    second commit

--- src/main/kotlin/Main3.kt
+++ src/main/kotlin/Main3.kt
@@ -1,18 +1,18 @@
-import com.sun.jdi.connect.Connector.StringArgument
-import java.util.Scanner
-
-fun countVowels(line: String):Int {
-    var count = 0
-    val check = arrayOf('a', 'e', 'i', 'o', 'u')
-    for(i in line)
-    {
-        if (i in check)
-            count += 1
-    }
-    return count
-}
-
-fun main() {
-    val input = Scanner(System.`in`)
-    println(countVowels(input.nextLine()))
-}
+//import com.sun.jdi.connect.Connector.StringArgument
+//import java.util.Scanner
+//
+//fun countVowels(line: String):Int {
+//    var count = 0
+//    val check = arrayOf('a', 'e', 'i', 'o', 'u')
+//    for(i in line)
+//    {
+//        if (i in check)
+//            count += 1
+//    }
+//    return count
+//}
+//
+//fun main() {
+//    val input = Scanner(System.`in`)
+//    println(countVowels(input.nextLine()))
+//}
